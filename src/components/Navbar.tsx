3c3e2b94a920b9002fbb430a0f4cc11015b471c3
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-02-12 10:56:38 +0100

    add active nav tab highlighting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
+import { useLocation } from "react-router-dom";
 import { NavLink } from "./NavLink"
 
 export const Navbar = () => {
+    const pathname  = useLocation();
+
     return (
         <header className="p-4 dark:bg-stone-800 dark:text-gray-100">
             <div className="container flex justify-between h-16 mx-auto md:justify-center md:space-x-8">
                 <ul className="items-stretch hidden space-x-3 md:flex">
-                    <NavLink title="Home" link="/" isActive={true} />
-                    <NavLink title="Create a DNS record" link="/new" isActive={false} />
-                    <NavLink title="List DNS records" link="/records" isActive={false} />
+                    <NavLink title="Home" link="/" isActive={pathname.pathname == "/" ? true : false} />
+                    <NavLink title="Create a DNS record" link="/new" isActive={pathname.pathname == "/new" ? true : false} />
+                    <NavLink title="List DNS records" link="/records" isActive={pathname.pathname == "/records" ? true : false} />
                 </ul>
             </div>
         </header>
