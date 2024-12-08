ff6d02bb3d9ab676c843e8bc60ccd54a2a59c2c0
Dominik Zarsky <dominik.zarsky@gmail.com>
2024-12-08 17:48:13 +0100

    Unify styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom';
-import { Button } from '../components/Button';
+import { Button } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
 	return (
 		<div className="mx-auto p-6 mt-5 rounded-lg w-2/3">
 			<h1 className="text-center text-3xl font-bold">Better WAPI</h1>
 			<p className="mt-2">
-				Better WAPI is a wrapper around the Wedos API (WAPI). Continue by create a new DNS record or listing
-				available records for a certain domain.
+				Better WAPI is a wrapper around the Wedos API (WAPI). Continue by create
+				a new DNS record or listing available records for a certain domain.
 			</p>
 			<div className="my-8 flex flex-row flex-auto justify-around w-1/2 mx-auto">
 				<Link to="/new">
-					<Button>Create a record</Button>
+					<Button color="primary" variant="ghost" size="lg">
+						Create a record
+					</Button>
 				</Link>
 				<Link to="/records">
-					<Button>List records</Button>
+					<Button color="primary" variant="ghost" size="lg">
+						List records
+					</Button>
 				</Link>
 			</div>
 		</div>
--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -1,22 +1,26 @@
-import type { UnknownAction } from '@reduxjs/toolkit';
-import { createRef, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import type { models_DeleteRowRequestV2, models_Record } from '../api';
-import type { RootState } from '../app/store';
-import { Button } from '../components/Button';
-import { DomainList } from '../components/DomainList';
-import { Login } from '../components/Login';
-import { UpdateForm } from '../components/UpdateForm';
-import { domainSlice } from '../redux/slices/domainSlice';
-import { deleteRecord } from '../redux/thunks/records/deleteRecord';
-import { fetchRecords } from '../redux/thunks/records/fetchRecords';
+import type { UnknownAction } from "@reduxjs/toolkit";
+import { createRef, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import type { models_DeleteRowRequestV2, models_Record } from "../api";
+import type { RootState } from "../app/store";
+import { DomainList } from "../components/DomainList";
+import { Login } from "../components/Login";
+import { UpdateForm } from "../components/UpdateForm";
+import { domainSlice } from "../redux/slices/domainSlice";
+import { deleteRecord } from "../redux/thunks/records/deleteRecord";
+import { fetchRecords } from "../redux/thunks/records/fetchRecords";
+import { Button, Input } from "@nextui-org/react";
+import { FiSearch } from "react-icons/fi";
 
 export const RecordsPage = () => {
 	const domain = useSelector((state: RootState) => state.domain);
-	const isUserLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+	const isUserLoggedIn = useSelector(
+		(state: RootState) => state.user.isLoggedIn,
+	);
 	const dispatch = useDispatch();
 
-	const [isUpdateScreenVisible, setUpdateScreenVisible] = useState<boolean>(false);
+	const [isUpdateScreenVisible, setUpdateScreenVisible] =
+		useState<boolean>(false);
 	const [editedSubdomain, setEditedSubdomain] = useState<models_Record>();
 
 	const domainRef = createRef<HTMLInputElement>();
@@ -38,7 +42,14 @@ export const RecordsPage = () => {
 		) {
 			dispatch(fetchRecords(activeDomain) as unknown as UnknownAction);
 		}
-	}, [activeDomain, dispatch, domain.subdomains.length, domain.error, isUserLoggedIn, domain.shouldFetchRecords]);
+	}, [
+		activeDomain,
+		dispatch,
+		domain.subdomains.length,
+		domain.error,
+		isUserLoggedIn,
+		domain.shouldFetchRecords,
+	]);
 
 	const handleDomainChange = async () => {
 		if (domainRef.current) {
@@ -52,10 +63,18 @@ export const RecordsPage = () => {
 			autocommit: false,
 		};
 
-		const confirm = window.confirm(`Are you sure you want to delete DNS record for ${subdomain}?`);
+		const confirm = window.confirm(
+			`Are you sure you want to delete DNS record for ${subdomain}?`,
+		);
 
 		if (confirm) {
-			dispatch(deleteRecord({ domain: domain.domain, request: request, id: id }) as unknown as UnknownAction);
+			dispatch(
+				deleteRecord({
+					domain: domain.domain,
+					request: request,
+					id: id,
+				}) as unknown as UnknownAction,
+			);
 		}
 	};
 
@@ -66,18 +85,22 @@ export const RecordsPage = () => {
 
 	return isUserLoggedIn ? (
 		<div className="text-center rounded-lg w-3/4 mx-auto items-center justify-center my-6 p-4">
-			<p>{!activeDomain ? 'Select domain to list records' : `Listing records for domain ${activeDomain}`}</p>
-			<div className="flex items-center justify-center space-x-4">
-				<input
+			<p>
+				{!activeDomain
+					? "Select domain to list records"
+					: `Listing records for domain ${activeDomain}`}
+			</p>
+			<div className="flex items-center justify-center space-x-4 mt-2">
+				<Input
+					size="lg"
 					type="text"
-					className="items-center p-5 w-3/4 mt-2 text-center rounded focus:outline-none focus:ring-2 focus:border-indigo-400 focus:ring-indigo-400 bg-gray-600"
-					ref={domainRef}
 					id="search"
+					ref={domainRef}
 					defaultValue={activeDomain}
 				/>
-				<span className="mt-2">
-					<Button onClick={handleDomainChange}>Search</Button>
-				</span>
+				<Button size="lg" variant="ghost" onClick={handleDomainChange}>
+					Search
+				</Button>
 			</div>
 
 			<DomainList
