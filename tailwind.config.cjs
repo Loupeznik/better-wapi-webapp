a9a789ecfbcccd509407d0a8137f5ffcaaf334fb
Dominik Zarsky <dominik.zarsky@gmail.com>
2025-12-13 16:40:34 +0100

    Remove borders from HeroUI components by setting borderWidth to 0px

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,8 +13,21 @@ module.exports = {
 	darkMode: "class",
 	plugins: [
 		heroui({
-			defaultTheme: "dark", // default theme from the themes object
-			defaultExtendTheme: "dark", // default theme to extend on custom themes
+			defaultTheme: "dark",
+			defaultExtendTheme: "dark",
+			layout: {
+				disabledOpacity: 0.3,
+				radius: {
+					small: "4px",
+					medium: "6px",
+					large: "8px",
+				},
+				borderWidth: {
+					small: "0px",
+					medium: "0px",
+					large: "0px",
+				},
+			},
 		}),
 	],
 };
