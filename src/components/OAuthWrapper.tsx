a19bc656aff2407a135cf654b9451afb7b21b62a
Dominik Zarsky <dominik.zarsky@gmail.com>
2025-12-13 16:33:32 +0100

    Add OAuth session state sync between oidc-react and Redux

--- src/components/OAuthWrapper.tsx
+++ src/components/OAuthWrapper.tsx
@@ -1,8 +1,24 @@
 import { useAuth } from "oidc-react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { OpenAPI } from "../api";
+import type { RootState } from "../app/store";
+import { userSlice } from "../redux/slices/userSlice";
 import MainLayout from "./MainLayout";
 
 function OAuthWrapper() {
 	const auth = useAuth();
+	const dispatch = useDispatch();
+	const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+
+	useEffect(() => {
+		if (auth.userData && !isLoggedIn) {
+			OpenAPI.TOKEN = auth.userData.access_token;
+			dispatch(userSlice.actions.loginOAuth());
+		} else if (!auth.userData && isLoggedIn) {
+			dispatch(userSlice.actions.logoutUser());
+		}
+	}, [auth.userData, isLoggedIn, dispatch]);
 
 	return <MainLayout auth={auth} />;
 }
